2cf2965b5cb271933c2dce0eeecedbe009053268
hariprasath <sivanandam.h@husky.neu.edu>
2018-09-24 21:36:13 -0400

    added html page for user-admin

---
+++ src/main/java/edu/northeastern/cs5610/WebdevF18s1UserAdminApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevF18s1UserAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevF18s1UserAdminApplication.class, args);
+	}
+}
